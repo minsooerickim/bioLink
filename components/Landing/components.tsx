30db41c5735e1c591e4366978ba2023b21217062
msclal <mika1shanela@gmail.com>
2023-05-21 06:43:10 -0700

    update landing text

--- components/Landing/components.tsx
+++ components/Landing/components.tsx
@@ -1,14 +1,13 @@
 export function Jumbotron() {
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      <div className="py-8 px-20 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl px-40 dark:text-white">
           Personalized Medical Diagnoses in Seconds!
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-          Unlocking Your Path to Optimal Health: Empowering You with
-          Cutting-Edge Technology and Unparalleled Expertise for Comprehensive,
-          Personalized, and Timely Medical Diagnoses
+          Unlocking your path to optimal health with cutting-edge technology and
+          unparalleled expertise for curated and timely medical diagnoses.
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
