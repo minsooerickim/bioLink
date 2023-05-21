1cb70e8b6bed61492228703a4ce7144c2b81540e
msclal <mika1shanela@gmail.com>
2023-05-21 07:01:58 -0700

    ad about us page

--- components/Landing/components.tsx
+++ components/Landing/components.tsx
@@ -2,7 +2,7 @@ export function Jumbotron() {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-20 mx-auto max-w-screen-xl text-center lg:py-16">
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl px-40 dark:text-white">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-7xl px-40 dark:text-white">
           Personalized Medical Diagnoses in Seconds!
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
---
+++ pages/about.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div className="w-full gap-y-0 flex flex-col justify-center items-start text-2xl p-40">
+      <h1 className="font-extrabold tracking-tight leading-none text-gray-900 text-7xl dark:text-white mb-8">
+        What is bioLink?
+      </h1>
+      <div className="">
+        <p className="mb-8 text-xl font-normal text-black">
+          Welcome to a realm where your well-being takes center stage. Prepare
+          to unlock the path to your ultimate health potential like never
+          before. At our forefront, we revolutionize the way healthcare embraces
+          you. Harnessing state-of-the-art technology and unmatched expertise,
+          we introduce a new era of curated, comprehensive, and time-sensitive
+          medical diagnoses.
+        </p>
+        <p className="mb-8 text-xl font-normal text-black">
+          Step into a world where personalized healthcare becomes a captivating
+          reality. We are here to guide you through this transformative journey
+          towards optimal well-being. Our pioneering use of cutting-edge
+          technology redefines the boundaries of healthcare possibilities.
+          Combined with our unrivaled expertise, we present a remarkable fusion
+          that delivers tailored, meticulous, and all-encompassing medical
+          diagnoses.
+        </p>
+        <p className="mb-8 text-xl font-normal text-black">
+          Say goodbye to prolonged waiting times and hello to accelerated
+          healthcare insights. Our commitment to promptness ensures that your
+          medical diagnoses are efficiently delivered, empowering you to take
+          informed action swiftly. No more uncertainty; its time to reclaim
+          control over your health with precision and speed.
+        </p>
+        <p className="mb-8 text-xl font-normal text-black">
+          Join us on this extraordinary adventure, where the future of
+          personalized healthcare unfolds before your eyes. Prepare to embark on
+          a captivating expedition towards optimal health – a realm where
+          cutting-edge technology and unparalleled expertise converge to curate,
+          comprehensively assess, and promptly reveal the secrets to your
+          well-being.
+        </p>
+        <p className="mb-8 text-xl font-normal text-black">
+          Are you ready to seize the key to your best self? Step forward and
+          embrace this groundbreaking experience today. Together, lets unlock
+          the door to your limitless health potential.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default about;
